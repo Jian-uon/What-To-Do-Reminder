4b41bfc78a6a62fffb7b6bd332f0d267fa15652e
usedrose <swjtu_hj@163.com>
2018-11-13 22:38:35 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 MAIL_TO_LIST=[
         {'name':'HuJian', 'email':'swjtu_hj@163.com'},
+        {'name':'Huzi', 'email':'735342728@qq.com'},
+        {'name':'Zihan', 'email':'490233575@qq.com'},
         ]
 MAIL_HOST = 'smtp.163.com'
 MAIL_USER = 'swjtu_hj@163.com'
